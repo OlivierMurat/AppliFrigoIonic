bb1255845ddb3002f48fcbf5ee832ab43d1bafa9
OlivierMurat <olivier.murat@ynov.com>
2018-06-22 01:22:32 +0200

    nouvelle liste

--- src/pages/liste/liste.ts
+++ src/pages/liste/liste.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController, ViewController, Platform } from 'ionic-angular';
 import { ListeDetailPage } from '../liste-detail/liste-detail';
 import { ListingApiProvider } from "../../providers/listing-api/listing-api";
+import { Ingredient } from '../../interfaces/ingredient';
 
 @IonicPage()
 @Component({
@@ -42,14 +43,38 @@ export class ListePage {
 })
 export class ModalListContentPage {
 
+  aliments: Ingredient[];
+
   constructor(
     public platform: Platform,
     public params: NavParams,
     public viewCtrl: ViewController
   ) {
+    this.aliments = [];
   }
 
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+
+  More(item) {
+    item.quantity += 1;
+  }
+
+  Less(item) {
+    item.quantity -= 1;
+  }
+
+  Delete(item) {
+    var index = this.aliments.indexOf(item, 0);
+    if (index > -1) {
+      this.aliments.splice(index, 1);
+    }
+  }
+
+  Add(name, quantity, unit) {
+    quantity = +quantity;
+    this.aliments.push({ id, name, quantity, unit });
+    console.log(this.aliments)
   }
 }
