6eaf47a9347b4f865ee715ab06c58fe438f9ffda
Clément Fleury <clem_ios@hotmail.com>
2018-06-22 00:52:55 +0200

    y voir plus clair

--- src/pages/liste/liste.module.ts
+++ src/pages/liste/liste.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { ListePage } from './liste';
+import { ListePage, ModalListContentPage } from './liste';
 
 @NgModule({
   declarations: [
     ListePage,
+    ModalListContentPage
   ],
   imports: [
     IonicPageModule.forChild(ListePage),
   ],
+  entryComponents: [
+    ModalListContentPage,
+  ]
 })
 export class ListePageModule {}
--- src/pages/liste/liste.ts
+++ src/pages/liste/liste.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, ViewController, Platform } from 'ionic-angular';
 import { ListeDetailPage } from '../liste-detail/liste-detail';
 import { ListingApiProvider } from "../../providers/listing-api/listing-api";
 
@@ -11,24 +11,45 @@ import { ListingApiProvider } from "../../providers/listing-api/listing-api";
 export class ListePage {
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private ListingApiProvider: ListingApiProvider) {
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public navParams: NavParams, private ListingApiProvider: ListingApiProvider) {
   }
 
   GoToDetail(liste) {
     this.navCtrl.push(ListeDetailPage, { liste: liste });
   }
 
+  presentAddModal() {
+    const modal = this.modalCtrl.create(ModalListContentPage);
+    modal.present();
+  }
+
   Delete(item) {
     this.ListingApiProvider.delete(item);
   }
 
-  AddListe(name, quantity, unit) {
+  AddListe(name, ingredients) {
     let id = Math.random() + Math.random();
-    this.ListingApiProvider.add({ id, name, quantity, unit });
+    this.ListingApiProvider.add({ id, name, ingredients });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RecettesPage');
   }
 
+}
+@Component({
+  templateUrl: 'addmodal.html'
+})
+export class ModalListContentPage {
+
+  constructor(
+    public platform: Platform,
+    public params: NavParams,
+    public viewCtrl: ViewController
+  ) {
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
 }
